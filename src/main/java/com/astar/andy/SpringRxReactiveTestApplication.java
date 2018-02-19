db3069546bd4a8d3e5c558c8e5fad27c6b68f84d
Andy Faust <andyfaust@Andys-MacBook-Pro.local>
2018-02-19 14:29:01 -0500

    Initial Project Setup

---
+++ src/main/java/com/astar/andy/SpringRxReactiveTestApplication.java
@@ -0,0 +1,12 @@
+package com.astar.andy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRxReactiveTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRxReactiveTestApplication.class, args);
+	}
+}
